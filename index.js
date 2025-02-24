b72e89f67611e6b2e6b83322eb66c18976ec1d0b
Abednego Alex <abednegoalex1@gmail.com>
2025-02-24 13:54:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config(); // Load API key from .env
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));   
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views")); 
